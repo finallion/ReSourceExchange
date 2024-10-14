70a839f2db8fbc7789290ec9ad1bfe3f029c4386
Finallion <74151350+finallion@users.noreply.github.com>
2024-10-14 13:18:16 +0200

    renaming!

--- src/main/java/com/resexchange/app/ResourceExchangeApplication.java
+++ src/main/java/com/resexchange/app/ResourceExchangeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class ResourceExchangeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(ResourceExchangeApplication.class, args);
 
 		// Benutzer-Name: user
 		// Password: beim Starten der Application in der Console
